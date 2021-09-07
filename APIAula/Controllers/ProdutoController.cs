2d9e06bbc5a74c471102d962b6bf0e00c5d21599
Home <caioroque879@gmail.com>
2021-09-07 17:23:50 -0300

    CRUD pronto

--- APIAula/Controllers/ProdutoController.cs
+++ APIAula/Controllers/ProdutoController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using API.Models;
@@ -19,27 +20,42 @@ namespace API.Controllers
         //POST: /api/produto/create
         [Route("create")]
         [HttpPost]
-        public Produto Create(Produto produto){
+        public IActionResult Create([FromBody] Produto produto){
 
             _context.Produtos.Add(produto);
             _context.SaveChanges();
-            return produto;
+            return Created("", produto);
         }
         //POST: /api/produto/list
         [Route("list")]
         [HttpGet]
-        public List<Produto> list() => _context.Produtos.ToList();
+        public IActionResult list() => Ok(_context.Produtos.ToList());
 
 
         [Route("update")]
         [HttpPost]
         //POST: /api/produto/delete
-        public Produto Update(Produto produto){
+        public IActionResult Update(Produto produto){
             _context.Produtos.Update(produto);
             _context.SaveChanges();
-            return produto;
+            return Ok();
         }
 
+        [Route("getbyid/{id}")]
+        [HttpGet]
+        //POST: /api/produto/getbyid
+        public IActionResult GetById([FromRoute] int id){
+            //buscar um objeto pela chave primaria
+            Produto produto = _context.Produtos.Find(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            return Ok(produto);
+        }
+
+        //Deletar por Id
+        /*
         [Route("delete")]
         [HttpPost]
         //POST: /api/produto/delete
@@ -47,6 +63,20 @@ namespace API.Controllers
             _context.Produtos.Remove(produto);
             _context.SaveChanges();
             return produto;
+        }*/
+
+        [Route("delete/{name}")]
+        [HttpDelete]
+        //POST: /api/produto/delete
+        public IActionResult Delete(string name){
+            Produto produto = _context.Produtos.FirstOrDefault(produto => produto.Nome == name);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+            return Ok(_context.Produtos.ToList());
         }
     }
 }
